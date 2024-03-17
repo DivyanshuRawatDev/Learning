2e08da177ce8ff38e445b38b408defa48b230506
Divyanshu766 <divyanshu.rawat.dev@gmail.com>
2024-03-17 21:00:07 +0530

    Socket Done

--- Socket/client/src/App.jsx
+++ Socket/client/src/App.jsx
@@ -1,20 +1,102 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import { io } from "socket.io-client";
+import { Button, Container, Stack, TextField, Typography } from "@mui/material";
 
 function App() {
-  const socket = io("http://localhost:3000");
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [roomName, setRoomName] = useState("");
+
+  const socket = useMemo(() => io("http://localhost:3000"), []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", { message, room });
+  };
+
+  const joinRoomHandler = (e) => {
+    e.preventDefault();
+    socket.emit("join-room", roomName);
+  };
 
   useEffect(() => {
     socket.on("connect", () => {
+      setRoom(room);
       console.log("Connected", socket.id);
     });
 
-    socket.on("welcome", (e) => {
-      console.log(e);
+    // socket.on("welcome", (e) => {
+    //   console.log(e);
+    // });
+
+    socket.on("recive-message", (data) => {
+      console.log(data);
+      setMessages((messages) => [...messages, data]);
     });
+
+    return () => {
+      socket.disconnect();
+    };
   }, []);
-  return <></>;
+  return (
+    <>
+      <Container maxWidth="sm">
+        <Typography varient="h1" component="div" gutterBottom>
+          Welcome to socket io
+        </Typography>
+
+        <Typography varient="h1" component="div" gutterBottom>
+          {socket.id}
+        </Typography>
+
+        <form onSubmit={joinRoomHandler}>
+          <h5>Join Room</h5>
+          <TextField
+            value={roomName}
+            onChange={(e) => setRoomName(e.target.value)}
+            id="outlined-basic"
+            label="Room Name"
+            varient="outlined"
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Join
+          </Button>
+        </form>
+
+        <form onSubmit={handleSubmit}>
+          <TextField
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            id="outlined-basic"
+            label="Message"
+            varient="outlined"
+          />
+          <TextField
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+            id="outlined-basic"
+            label="Room"
+            varient="outlined"
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Send
+          </Button>
+        </form>
+
+        <Stack>
+          {messages.map((m, i) => {
+            return (
+              <Typography key={i} variant="h6" component="div" gutterBottom>
+                {m}
+              </Typography>
+            );
+          })}
+        </Stack>
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- Socket/server/app.js
+++ Socket/server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
 import cors from "cors";
+import jwt from "jsonwebtoken";
 
 const port = 3000;
 
@@ -28,11 +29,43 @@ app.get("/", (req, res) => {
   res.send("Running");
 });
 
+const secretKey = "asdasdasdasd";
+app.get("/login", (req, res) => {
+  const token = jwt.sign({ _id: "asssdsasdasd" }, secretKey);
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: true,
+    })
+    .json({
+      message: "Login Success",
+    });
+});
+
+//Middleware :-
+io.use((socket, next) => {});
+
 io.on("connection", (socket) => {
   console.log("User Connected");
   console.log(socket.id);
 
-  socket.emit("welcome", `Welcome ${socket.id}`);
+  //   socket.emit("welcome", `Welcome ${socket.id}`); //Perticular person get data we can take exaple of whatsapp when group join it will give us message you joined a group
+  //   socket.broadcast.emit("welcome", `Welcome ${socket.id}`); //All other will get message except this one this will send to another one that divyanshu joined the group
+
+  socket.on("message", ({ room, message }) => {
+    // console.log(data);
+    io.to(room).emit("recive-message", message);
+  });
+
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(room);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
 });
 
 server.listen(3000, () => {
